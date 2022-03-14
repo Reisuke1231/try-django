3f20355f802c56b5015e05f83d574d1a26fcb256
Reisuke1231 <reisuke1231@gmail.com>
2022-03-14 09:19:47 +0900

    Add global template

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,19 +1,22 @@
 """
 To render html web pages
 """
-
-from django.http import HttpResponse
 import random
-
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 from articles.models import Article
 
 random_id = random.randint(1, 4)
 
 article_obj = Article.objects.get(id=random_id)
 
-HTML_STRING = f"""
-<h1>{article_obj.title} - id:{article_obj.id}</h1>
-"""
+context = {
+    'title': article_obj.title,
+    'id': article_obj.id,
+    'content': article_obj.content
+}
+
+HTML_STRING = render_to_string('home-view.html', context=context)
 
 
 def home_view(request):
